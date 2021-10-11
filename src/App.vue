7986ff4947c762810f4cefb0285852657c67f306
Filip <filip.knias@o2.pl>
2021-10-11 12:52:14 +0200

    Setup scss styles and container div

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
+import "./styles/index.scss";
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  overflow-y: none;
 }
 </style>
